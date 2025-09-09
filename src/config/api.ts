fc8b89b991b1a5ef626856043873d228c34d03a7
carlosrmfernandes25 <carlos.rafael@picpay.com>
2025-09-09 09:18:58 -0300

    ook

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-  // BASE_URL: 'https://aviation-certs-api.onrender.com',
-  BASE_URL: 'http://127.0.0.1:8000'
+  BASE_URL: 'https://aviation-certs-api.onrender.com',
+  //BASE_URL: 'http://127.0.0.1:8000'
 };
 
 export const getApiUrl = (endpoint: string): string => {
